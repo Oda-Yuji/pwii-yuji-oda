47887c1e0fb17fbd81c469920d52c54b0c3a21ea
Yuji Oda <yujioda1703@gmail.com>
2025-02-26 09:07:14 -0300

    Aula 1

---
+++ aula01/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        .texto 
+        {
+            size: 5rem;
+            color: red;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+
+    <body>
+        <p class="texto">
+            <?php
+                echo "Oi";
+            ?>
+        </p>
+    </body>
+</html>
