07f266ebfe26add501bbe9f34d32087a75bbd772
Yuji Oda <yujioda1703@gmail.com>
2025-02-26 09:25:13 -0300

    Aula 2

--- aula01/home.php
+++ aula01/home.php
@@ -15,5 +15,10 @@
                 echo "Oi";
             ?>
         </p>
+        <a href="https://www.ecycle.com.br/abacate/#:~:text=Abacate%20%C3%A9%20um%20pseudofruto%20comest%C3%ADvel,saud%C3%A1vel%20e%20do%20bem%2Destar.">
+            <?php
+                echo "cu";
+            ?>
+        </a>
     </body>
 </html>
---
+++ aula02/concatenar.php
@@ -0,0 +1,12 @@
+<?php
+    ECHO "Hello wolrd!";
+    echo "Hello wolrd!";
+    EcHo "Hello wolrd!";
+
+    $nome = "Yuji";
+    $sobrenomne = "Oda";
+    $numero = 4;
+
+    echo "${nome} <h1> ${sobrenome} </h1>";
+    echo $numero . "<p> " . $nome . " " . $sobrenomne . "</p>";
+?>
